e8d5da70537ffb0f3bb82a1c33ce951632b882c8
Ammar <ammardan.5d@gmail.com>
2022-06-29 17:04:23 +0200

    email sending implemented

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Contact.css";
+import emailjs from "emailjs-com";
 import { GiModernCity } from "react-icons/gi";
 import { BiCurrentLocation } from "react-icons/bi";
 import { HiOutlineMail } from "react-icons/hi";
@@ -7,25 +8,55 @@ import { BiMobileAlt } from "react-icons/bi";
 import FooterImage from "../Layout/Navbar/images/logo.svg";
 
 function Contact() {
+  const [emailSent, setEmailSent] = useState("");
+  const [emailSentStaus, setEmailStatus] = useState(false);
+
+  const sendEmail = (e) => {
+    setEmailSent(<h3 className="email_status_pending eResult">Sending...</h3>);
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_nt23vph",
+        "template_o4hb4w8",
+        e.target,
+        "jrMElmmillptPAm8a"
+      )
+      .then((res) => {
+        setEmailStatus(true);
+        setEmailSent(
+          <h3 className="email_status_success eResult">
+            Email sent successfully
+          </h3>
+        );
+      })
+      .catch((err) => {
+        setEmailStatus(false);
+        setEmailSent(
+          <h3 className="email_status_faild eResult">Email Faild</h3>
+        );
+      });
+  };
   return (
     <section className="contact" id="contact">
-      <div className="email_container">
-        <label for="fname">Your Name</label>
-        <input type="text" id="fname" name="fname" />
+      <form onSubmit={sendEmail} className="email_container">
+        <label htmlFor="fname">Your Name</label>
+        <input type="text" id="fname" name="fname" required />
 
-        <label for="email">Your Email</label>
-        <input type="text" id="email" name="email" />
+        <label htmlFor="user_email">Your Email</label>
+        <input type="email" id="user_email" name="user_email" required />
 
-        <label for="message">Your Message</label>
+        <label htmlFor="message">Your Message</label>
         <textarea
           type="text"
           id="message"
           name="message"
           placeholder="write your message"
+          required
         />
 
         <button className="email_btn">Send</button>
-      </div>
+        {emailSent}
+      </form>
       <div className="contact_infos">
         <h3 className="contact_title">Contact</h3>
         <div className="personal_info">
