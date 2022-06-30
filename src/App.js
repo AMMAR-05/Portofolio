d8372af0f104b0553b2509a2ec484bc0bcfc6ec6
Ammar <ammardan.5d@gmail.com>
2022-06-30 06:07:59 +0200

    Loading Page implemented

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
+import { useEffect, useState } from "react";
 import Navbar from "./components/Layout/Navbar/Navbar";
 import Intro from "./components/Introduction/Intro";
 import Projects from "../src/components/Projects/Projects";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
-import Footer from "./components/Layout/Footer/Footer";
+// import Footer from "./components/Layout/Footer/Footer";
 
 function App() {
+  const [homeIsLoading, setHomeIsLoading] = useState(false);
+
+  useEffect(() => {
+    setHomeIsLoading(true);
+    setTimeout(() => {
+      setHomeIsLoading(false);
+    }, 7000);
+  }, []);
   return (
     <div className="App">
+      {homeIsLoading && (
+        <div className="loading_bg">
+          <h2 className="animate_charcter">
+            <span className="text">DO SOMETHING GREAT</span>
+          </h2>
+        </div>
+      )}
       {/* Navbar */}
       <Navbar />
 
       {/* Intro */}
+
       <Intro />
 
       {/* Projects */}
